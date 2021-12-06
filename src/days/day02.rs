653b936c4f0c30a199be0a47d3d3202891819e3f
Joonas Loppi <joonas@joonas.fi>
2021-12-06 15:38:45 +0200

    add 2nd part of day 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -8,7 +8,26 @@ pub fn a() {
     for command_text in command_texts.lines() {
         let command = command_parse(command_text.to_string()).unwrap();
 
-        position = command_apply(command, position)
+        position = command_apply_a(command, position)
+    }
+
+    println!(
+        "horizontal={} depth={} multiplied={}",
+        position.horizontal,
+        position.depth,
+        position_multiply(&position),
+    );
+}
+
+pub fn b() {
+    let command_texts = fs::read_to_string("day02_input.txt").unwrap();
+
+    let mut position = position_zero();
+
+    for command_text in command_texts.lines() {
+        let command = command_parse(command_text.to_string()).unwrap();
+
+        position = command_apply_b(command, position)
     }
 
     println!(
@@ -28,12 +47,14 @@ enum Command {
 struct Position {
     horizontal: i32,
     depth: i32,
+    aim: i32,
 }
 
 fn position_zero() -> Position {
     Position {
         horizontal: 0,
         depth: 0,
+        aim: 0,
     }
 }
 
@@ -54,19 +75,42 @@ fn command_parse(comm_str: String) -> Result<Command, String> {
     }
 }
 
-fn command_apply(command: Command, current: Position) -> Position {
+fn command_apply_a(command: Command, current: Position) -> Position {
     match command {
         Command::Up(amount) => Position {
             horizontal: current.horizontal,
             depth: current.depth - amount,
+            aim: current.aim, // unchanged in first part
         },
         Command::Down(amount) => Position {
             horizontal: current.horizontal,
             depth: current.depth + amount,
+            aim: current.aim, // unchanged in first part
         },
         Command::Forward(amount) => Position {
             horizontal: current.horizontal + amount,
             depth: current.depth,
+            aim: current.aim, // unchanged in first part
+        },
+    }
+}
+
+fn command_apply_b(command: Command, current: Position) -> Position {
+    match command {
+        Command::Down(amount) => Position {
+            horizontal: current.horizontal,
+            depth: current.depth,
+            aim: current.aim + amount,
+        },
+        Command::Up(amount) => Position {
+            horizontal: current.horizontal,
+            depth: current.depth,
+            aim: current.aim - amount,
+        },
+        Command::Forward(amount) => Position {
+            horizontal: current.horizontal + amount,
+            depth: current.depth + (current.aim * amount),
+            aim: current.aim,
         },
     }
 }
@@ -76,7 +120,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_a() {
         let test_input = "forward 5
 down 5
 forward 8
@@ -90,12 +134,36 @@ forward 2
         for line in test_input.lines() {
             let command = command_parse(line.to_string()).unwrap();
 
-            position = command_apply(command, position)
+            position = command_apply_a(command, position)
         }
 
         assert_eq!(position.horizontal, 15);
         assert_eq!(position.depth, 10);
 
         assert_eq!(position_multiply(&position), 150);
+    }
+
+    #[test]
+    fn test_b() {
+        let test_input = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+";
+
+        let mut position = position_zero();
+
+        for line in test_input.lines() {
+            let command = command_parse(line.to_string()).unwrap();
+
+            position = command_apply_b(command, position)
+        }
+
+        assert_eq!(position.horizontal, 15);
+        assert_eq!(position.depth, 60);
+
+        assert_eq!(position_multiply(&position), 900);
     }
 }
